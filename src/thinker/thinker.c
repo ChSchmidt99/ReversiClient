34bd290b466620652f03d3db90c84413d3300dac
f <fleance@kitpvp.de>
2019-11-28 13:17:44 +0100

    Trying different tick method

--- src/thinker/thinker.c
+++ src/thinker/thinker.c
@@ -6,40 +6,18 @@
 
 void tick(char* shm, int pipe[]) {
     close(pipe[0]);
-    sigset_t mask, oldMask;
 
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGUSR1);
-    sigprocmask(SIG_BLOCK, &mask, &oldMask);
-
-    printf("Thinker is now waiting for signals (SIGUSR1)\n");
-    int f = 0;
-    while(f++ < 100) {
-        sigsuspend(&oldMask);
-        sigprocmask(SIG_UNBLOCK, &mask, NULL);
-
-        printf("Got signal from communicator, waking up..");
-        printf("Loading current board");
-        printf("%s", shm);
-        /*
-        Field* field = createField(8, 8);
-        loadFieldFromSHM(field, shm);
-        long int x = -1, y = -1;
-        for(unsigned int j = 0; j < field->width; j++) {
-            for(unsigned int i = 0 ; i < field->height; i++) {
-                if(isValidMove(field, 'W', j, i)) {
-                    x = j;
-                    y = i;
-                    break;
-                }
-            }
-        }
-
-        printf("Found available move: %li %li", x, y);
-
-        char* move = toString(x, y);
-        write(pipe[1], move, sizeof(move) + 1);
-        free(move);*/
-        printf("Sent move to connector, sleeping again");
+    sigset_t sigset;
+    int sig;
+    sigemptyset(&sigset);
+    if(sigaddset(&sigset, SIGUSR1) == -1) {
+        perror("sigaddset");
     }
+    printf("Thinker is now waiting for signals (SIGUSR1)\n");
+    if(sigwait(&sigset, &sig) != 0) {
+        perror("sigwait");
+    }
+    printf("Got signal, exiting..\n");
+    if(shm != NULL)
+        printf("%s", shm);
 }
