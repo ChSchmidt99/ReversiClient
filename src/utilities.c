09b942c3b0b1aa56023c0cd5feded83a356e4b3b
f <fleance@kitpvp.de>
2019-12-02 15:25:29 +0100

    Add debug to check slicing works correctly

--- src/utilities.c
+++ src/utilities.c
@@ -27,6 +27,7 @@ char** slice(const char* str, char delimiter[]) {
     char* copy = copyStringToNewMemoryAddr(str);
     char** result = NULL;
     char* token = strtok(copy, delimiter);
+    printf("slicing string '%s' with delimiter '%s'\n", copy, delimiter);
     int delimiterC = 0;
 
     while(token) {
