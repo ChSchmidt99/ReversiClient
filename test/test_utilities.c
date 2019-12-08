dd2f482de82b5978fb10116390819a6e7f75b7b0
ChristianBlueGo <chn.schmidt23@gmail.com>
2019-12-08 11:26:19 +0100

    added slice test

--- test/test_utilities.c
+++ test/test_utilities.c
@@ -11,8 +11,26 @@ static char * testConcatStringsToNewMemoryAddr() {
     return 0;
 }
 
+static char * test_copyStringToNewMemoryAddr() {
+    char* baseString = "3,2";
+    char* newString = copyStringToNewMemoryAddr(baseString);
+    mu_assert("Strings should be Equal", strcmp(baseString,newString) == 0);
+    free(newString);
+    return 0;
+}
+
+static char * test_Slice() {
+    size_t length = 0;
+    char** tokens = slice("Hallo ich bin ein toller test"," ",&length);
+    mu_assert("Should have 6 tokens", length == 6);
+    mu_assert("First Token should be Hallo", strcmp(tokens[0],"Hallo") == 0);
+    return 0;
+}
+
 static char * utilities_tests() {
     mu_run_test(testConcatStringsToNewMemoryAddr);
+    mu_run_test(test_copyStringToNewMemoryAddr);
+    mu_run_test(test_Slice);
     return 0;
 }
  
