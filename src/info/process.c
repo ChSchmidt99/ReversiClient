36a35abd948286a40455331acce821265cf005fc
f <fleance@kitpvp.de>
2019-12-10 17:47:38 +0100

    Debugging "Alloc memory correctly for ProcessInfo"

--- src/info/process.c
+++ src/info/process.c
@@ -8,9 +8,9 @@ ProcessInfo* createProcessInfo() {
     if(pipe(fd) < 0)
         panic("Could not create a pipe");
 
-    ProcessInfo* info = malloc(sizeof(ProcessInfo) + sizeof(pid_t*) * 2);
-    info->fd[0] = fd[0];
-    info->fd[1] = fd[1];
+    ProcessInfo* info = malloc(sizeof(ProcessInfo));
+    //info->fd[0] = fd[0];
+    //info->fd[1] = fd[1];
     *info->child = -1;
     *info->parent = -1;
 
