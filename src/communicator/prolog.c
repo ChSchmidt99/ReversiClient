ef90018417f56556d03734c681b0c6b273f9fc41
f <fleance@kitpvp.de>
2019-12-10 12:54:34 +0100

    Change split limit

--- src/communicator/prolog.c
+++ src/communicator/prolog.c
@@ -64,7 +64,7 @@ PlayerInfo* initiateProlog(Connection* connection, const char* version, const ch
 
 Player* parseOwn(ServerMessage* message) {
     size_t length = 0;
-    char** data  = slice(message->clearText, " ", &length, 3);
+    char** data  = slice(message->clearText, " ", &length, -1);
     for(size_t i = 0; i < length; i++) {
         printf("%zu > '%s'\n", i, data[i]);
     }
